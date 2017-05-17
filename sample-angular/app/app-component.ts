a91da3d9eb75da8daa3df1e13abda25f2858f336
TomazMartins <tomaz.r.martins@gmail.com>
2017-05-17 11:58:17 -0300

    Implementing AppComponent class
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- sample-angular/app/app-component.ts
+++ sample-angular/app/app-component.ts
@@ -3,7 +3,27 @@ import{ Component } from 'angular2/core'
 @Component(
   {
     selector: "my-app",
-    template: "<h1> Bem Vindo ao Mundo Angular 2 e Typescript</h1>"
+    template: `
+      <h1>Bem Vindo ao Mundo Angular 2 e Typescript</h1>
+      <h2>Listagem de Tarefas</h2>
+      <ul>
+        <li *ngFor="#task of tasks" >
+          {{ task }}
+        </li>
+      </ul>
+      <input type="text" placeholder="Digit your task here..." [(ngModel)]="task" />
+      <button (click)="addTask()">Add Task</button>
+    `
   }
 )
-export class AppCompnent {}
+export class AppCompnent {
+  public tasks:string[] = [];
+  public task:string;
+
+  addTask() {
+    if( this.task != "" ) {
+      this.tasks.push( this.task );
+      this.task = "";
+    }
+  }
+}
