ba7979555a4bd89f9942fafb6ca79591b2180e02
TomazMartins <tomaz.r.martins@gmail.com>
2017-05-17 18:34:54 -0300

    Implementing models Student and Course
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ sample-angular/app/models/Course.ts
@@ -0,0 +1,6 @@
+export class Course {
+    constructor(
+      public id:string,
+      public name:string
+    ){}
+}
---
+++ sample-angular/app/models/Student.ts
@@ -0,0 +1,8 @@
+export class Student {
+    constructor(
+      public name?:string,
+      public email?:string,
+      public age?:number,
+      public course?:string
+    ){}
+}
