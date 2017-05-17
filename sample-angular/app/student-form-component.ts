748082ea609b364eb950f2455ff386e9b3cf87cc
TomazMartins <tomaz.r.martins@gmail.com>
2017-05-17 18:41:15 -0300

    Implementing StudentFormComponent
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- sample-angular/app/student-form-component.ts
+++ sample-angular/app/student-form-component.ts
@@ -1,4 +1,6 @@
 import{ Component } from "angular2/core";
+import{ Student } from "./models/student";
+import{ Course } from "./models/course";
 
 @Component (
   {
@@ -7,5 +9,28 @@ import{ Component } from "angular2/core";
   }
 )
 export class StudentFormComponent {
+  isSuccess:boolean;
 
+  public student:Student;
+  public courses:Course[];
+
+  constructor() {
+    this.courses = [
+      new Course( "cpp", "C++" ),
+      new Course( "java", "Java" ),
+      new Course( "android", "Android" ),
+      new Course( "rubyonrails", "Ruby On Rails" ),
+      new Course( "angular2", "Angular 2" )
+    ]
+
+    isSuccess = false;
+  }
+
+  onSubmit() {
+    this.isSuccess = true;
+  }
+
+  debug():string {
+    return JSON.stringify( this.student );
+  }
 }
