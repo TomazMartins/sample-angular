aebd424112a44e0aae5ef05b1154bc360e97e693
TomazMartins <tomaz.r.martins@gmail.com>
2017-05-16 23:13:31 -0300

    Implementing first component
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ sample-angular/app/app-component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component(
+  {
+    selector: "my-app",
+    template: "<h1> Bem Vindo ao Mundo Angular 2 e Typescript</h1>"
+  }
+)
+export class AppCompnent() {}
